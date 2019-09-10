74d1b311ec182c9246af58e0790f2b3d113dcbe2
Ajitem Sahasrabuddhe <ajitem.sahasrabuddhe@perennialsys.com>
2019-09-10 17:24:01 +0530

    implement basic logic

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"github.com/urfave/cli"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 func main() {
@@ -13,12 +17,12 @@ func main() {
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "source, s",
-			Value: "",
+			Value: "-",
 			Usage: "path to markdown file to parse for generating table of contents or '-' to read from stdin",
 		},
 		cli.StringFlag{
 			Name:  "destination, d",
-			Value: "",
+			Value: "-",
 			Usage: "path to markdown file to write after table of contents has been generated or '-' to write to stdout",
 		},
 		cli.BoolTFlag{
@@ -29,13 +33,8 @@ func main() {
 			Name:  "inline, i",
 			Usage: "appends the source file with the table of contents without creating a new file",
 		},
-		cli.StringSliceFlag{
-			Name:  "bullets, b",
-			Usage: "bullets to be used for items generated in the table of contents. multiple bullets also supported which would be used based on the depth of header",
-			Value: &cli.StringSlice{"*"},
-		},
 		cli.IntFlag{
-			Name:  "max-depth, d",
+			Name:  "max-depth, m",
 			Usage: "use headings with nested depth `n` to generate table of contents items",
 			Value: 6,
 		},
@@ -51,14 +50,80 @@ func main() {
 		destination := context.String("destination")
 		outputCompleteFile := context.Bool("output-complete-file")
 		inline := context.Bool("inline")
-		bullets := context.StringSlice("bullets")
 		depth := context.Int("max-depth")
 		skipHeaders := context.Int("skip-headers")
 
-		// read source
+		// get reader
+		reader, err := getSourceReader(source)
+		if err != nil {
+			return err
+		}
+		defer reader.Close()
+
+		// get source input
+		input, err := readFromSource(reader)
+		if err != nil {
+			return err
+		}
 
 		// generate toc
+		output, err := generateToc(input, depth, skipHeaders)
+		if err != nil {
+			return err
+		}
+
+		if outputCompleteFile {
+			// replace
+		}
+
+		// get writer
+		var writer io.WriteCloser
+		if inline {
+			writer, err = getSourceWriter(source)
+		} else {
+			writer, err = getSourceWriter(destination)
+		}
+		if err != nil {
+			return err
+		}
+		defer writer.Close()
 
 		// write output
+		err = writeToDestination(output, writer)
+		if err != nil {
+			return err
+		}
+
+		return nil
 	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getSourceReader(source string) (io.ReadCloser, error) {
+	if source == "-" {
+		return os.Stdin, nil
+	} else {
+		return os.Open(source)
+	}
+}
+
+func getSourceWriter(destination string) (io.WriteCloser, error) {
+	if destination == "-" {
+		return os.Stdout, nil
+	} else {
+		return os.Open(destination)
+	}
+}
+
+func readFromSource(reader io.Reader) ([]byte, error) {
+	return ioutil.ReadAll(reader)
+}
+
+func writeToDestination(output []byte, writer io.Writer) error {
+	_, err := writer.Write(output)
+	return err
 }
